b75cd6bd372299f5e93f25d73fb6b715f4504ffd
Mohssin <mosssinyo@gmail.com>
2022-10-17 23:32:10 +0200

    Añadida query PostGis para obtener un área
    Logica esquinas con array para cada una x,y

--- server-node-express/src/main.ts
+++ server-node-express/src/main.ts
@@ -8,7 +8,15 @@ const knex = require('../db/db')
 
 
 app.get('/', (req:Request, res:Response) => {
-    knex.select().from('worldtifmauto').limit(1).then((data:any) => {
+    const { topLeft, bottomRight, proj } = req.body
+
+    //Logica esquinas: topLeft[0 (x),1 (y)], bottomRight[0 (x),1 (y)] se dibuja el area a partir de esas esquinas.
+    knex.select(knex.raw(`(ST_PixelAsPolygons(st_union(st_clip(rast, geom)))).val as val, ST_AsText((ST_PixelAsPolygons(st_union(st_clip(rast, geom)))).geom) as geo`))
+    .from('worldtifmauto')
+    .crossJoin(knex.raw(`st_polygonfromtext('POLYGON((${topLeft[0]} ${topLeft[1]}, ${topLeft[0]} ${bottomRight[1]}, ${bottomRight[0]} ${bottomRight[1]}, ${bottomRight[0]} ${topLeft[1]}, ${topLeft[0]} ${topLeft[1]}))', ${proj}) as geom`))
+    .where(knex.raw(`st_intersects(rast, geom)`))
+    .groupby('rast')
+    .then((data:any) => {
         res.send(data)
     });
 })
